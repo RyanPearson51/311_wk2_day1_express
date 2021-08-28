b0454b6b227419d8c7195155846b19aaf7ba1c76
Ryan Pearson <ryanpearson015@gmail.com>
2021-08-28 15:51:52 -0500

    started assignment, initialized basic routes

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  return res.json(users);
+})
 
+app.get('/users/1', (req, res) => {
+  //need to do anything specific to only return one user?
+  //return res.json(users)
+})
+
+//app.post()
+
+//app.put()
+
+//app.delete()
 
 /* END - create routes here */
 
